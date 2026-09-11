8562a50108d15ced26d238bef79e9c53205994a7
XPS-home-wsl <pedrolp321@outlook.com>
2026-09-11 18:36:54 +0200

    more customization

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,6 +5,13 @@ git clone --bare git@github.com:svic321/dotfiles.git "$HOME/.dotfiles" \
   || git clone --bare https://github.com/svic321/dotfiles "$HOME/.dotfiles"
 git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" config --local status.showUntrackedFiles no
 git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" checkout
+if ! grep -Fqx '[include]' "$HOME/.gitconfig" 2>/dev/null || ! grep -Fqx '  path = ~/.svic321/git/.gitconfig' "$HOME/.gitconfig" 2>/dev/null; then
+  printf '%s\n' '[include]' '  path = ~/.svic321/git/.gitconfig' >> "$HOME/.gitconfig"
+fi
+if [[ ! -f "$HOME/.config/starship.toml" ]]; then
+  mkdir -p "$HOME/.config"
+  starship preset nerd-font-symbols -o "$HOME/.config/starship.toml"
+fi
 sudo rosdep update
 sudo rosdep install --from-paths src --ignore-src -y
 sudo chown -R "$(whoami)" /home/ws/
